86df95dc3c1c3ce3517f80328d5dc3ba18f9821c
Timothy Gaither <timjgaither@gmail.com>
2022-03-25 14:45:21 +0100

    push

---
+++ resources/views/sicurezza.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Discord</title>
+</head>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+header{
+   
+    height: 70px;
+    background-color:#3161e0;
+   
+ }
+.row{
+    height: 70px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+
+nav{
+    display: flex;  
+}
+
+nav ul{
+    justify-content: space-around;
+    display: flex;
+    list-style: none;
+
+}
+
+nav ul li{
+    margin: 0px 30px;
+}
+nav ul li a{
+    color: #000;
+}
+/* Regole bottone login */
+.login {
+    display: inline-block;
+    
+    width: 15%;
+    text-align: right;
+}
+
+.login .btn{
+    text-decoration: none;
+    background-color: white;
+    color: black;
+    border-radius: 16px;
+    padding: 7px 16px;
+    display: inline;
+
+}
+
+/* Fine regole header */
+
+.active{
+    color: red;
+}
+
+main h1{
+    margin-top: 50px;
+    text-align: center;
+    width: 100%;
+    font-size: 30px;
+}
+
+</style>
+<body>
+    <!-- Inizio header -->
+    <header>
+        <!-- Contenitore elementi presenti nel header -->
+        <div class="container">
+            <div class="row">
+                <!-- Barra di navigazione header -->
+                <nav>
+                    <ul>
+                        <li><a href="scarica">Scarica</a></li>
+                        <li><a href="home">Home Discord</a></li>
+                        <li><a href="nitro">Nitro</a></li>
+                        <li><a class="active" href="sicurezza">Sicurezza</a></li>
+                        <li><a href="assistenza">Assistenza</a></li>
+                    </ul>
+                </nav>
+
+                <!-- Tasto login per account -->
+                <div class="login">
+                    <a class="btn" href="#">Accedi</a>
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <!-- Fine header -->
+
+    <main>
+        <h1>Pagina SICUREZZA</h1>
+    </main>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,25 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/scarica', function () {
+    return view('scarica');
+});
+
+Route::get('/nitro', function () {
+    return view('nitro');
+});
+
+Route::get('/sicurezza', function () {
+    return view('sicurezza');
+});
+Route::get('/assistenza', function () {
+    return view('assistenza');
 });
